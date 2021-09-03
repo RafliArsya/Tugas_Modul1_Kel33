269a9da89bd038cd214ecac2a4b6f5df0bb428d6
RafliArsya <47909089+RafliArsya@users.noreply.github.com>
2021-09-03 22:23:06 +0700

    3 - Fix Wrong Upload

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-var a = 0;
-console.log("ini root scope: " + a);
-function q2() {
-    a = 5;
-    console.log("ini child scope: " + a);
+var myArray = [];
+var outArray = {};
+
+const addMerk = () => {
+    let input = document.getElementById("addMerk").value;
+    myArray.push(input.toUpperCase());
 }
-q2();
-console.log("ini root scope: " + a);
 
-var angka = [1,2,3];
-var dobel = angka.map(function(num){
-    return num * 2;
-});
-
-console.log(angka+"\n"+dobel);
-
-const myArray = [5,10,20];
-const reduces = myArray.reduce((total, jumlah)=>total + jumlah);
-
-console.log(reduces);
+const showMerk = () => {
+    let text = "";
+    //const outArr = myArray.reduce((idx, val) => idx.set(val, (idx.get(val) || 0) + 1), new Map());
+    /*const outArr = myArray.reduce((idx, val) => {
+        idx[val] = (idx[val] || 0) +1;
+        return idx;
+    }, {});*/
+    const outArr = myArray.reduce((idx, val) => idx.set(val, (idx.get(val) || 0) + 1), new Map());
+    outArr.forEach((item, idx) => { 
+        text += idx + ": " + item + "<br>";  
+    });
+    document.getElementById("outMerk").innerHTML = text;
+    //console.log(text);
+    //console.log(outArr);
+    //document.getElementById("outMerk").innerHTML = [...map.keys()];
+    //document.getElementById("outVal").innerHTML = [...map.values()];
+}

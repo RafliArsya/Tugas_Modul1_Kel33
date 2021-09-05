3115eabf0e75aa710de0638bc257ae242defd610
RafliArsya <47909089+RafliArsya@users.noreply.github.com>
2021-09-06 00:44:14 +0700

    8 - ADDDDD TERNARY OPSSSS

--- script.js
+++ script.js
@@ -2,21 +2,26 @@ var myArray = [];
 var outArray = {};
 
 const addMerk = () => {
-    let input = document.getElementById("addMerk").value;
-    myArray.push(input.toUpperCase());
+    let input = document.getElementById("addMerk").value.trim()
+    output = input.length > 0 ? myArray.push(input.toUpperCase()) : "";
+    let output;
 }
 
 const showMerk = () => {
-    let text = "";
+    let text = myArray.length === 0 ? "empty" : "";
+    
     //const outArr = myArray.reduce((idx, val) => idx.set(val, (idx.get(val) || 0) + 1), new Map());
     /*const outArr = myArray.reduce((idx, val) => {
         idx[val] = (idx[val] || 0) +1;
         return idx;
     }, {});*/
-    const outArr = myArray.reduce((idx, val) => idx.set(val, (idx.get(val) || 0) + 1), new Map());
-    outArr.forEach((item, idx) => { 
+    if (text != "empty") {
+        const outArr = myArray.reduce((idx, val) => idx.set(val, (idx.get(val) || 0) + 1), new Map());
+        outArr.forEach((item, idx) => { 
         text += idx + ": " + item + "<br>";  
-    });
+        });
+    }
+    
     document.getElementById("outMerk").innerHTML = text;
     //console.log(text);
     //console.log(outArr);
